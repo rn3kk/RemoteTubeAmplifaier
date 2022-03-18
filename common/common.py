1d441882b95e12e590654d8cf07db50dce25496b
nbanshchikov <nbanschikov@gmail.com>
2022-03-18 17:56:14 +0300

    Создание GUI клиента

--- common/common.py
+++ common/common.py
@@ -1,12 +1,32 @@
+import json
+
 COMMAND = "c"
 VALUE = "v"
-MECH_NAME = "m"
-
 
 CMD_AUTORISATION_TOKEN = 1
-CMD_CHANGE_ANGLE = 2
-CMD_CHANGE_PWR = 3
-CMD_CHANGE_RELAY = 4
+CMD_CHANGE_PWR = 2
+CMD_CHANGE_RELAY = 3
+CMD_CHANGE_ANGLE_MECH1 = 4
+CMD_CHANGE_ANGLE_MECH2 = 5
 
-FROM_PA_ANGLE = 1
-FROM_PA_MANUAL_MODE = 2
+FROM_PA_MANUAL_MODE = 1
+FROM_PA_ANGLE_MECH1 = 2
+FROM_PA_ANGLE_MECH2 = 3
+
+class Protocol:
+
+    @staticmethod
+    def parseCmd(data):
+        if type(data) is str or type(data) is bytes:
+            json_cmd = json.loads(data)
+        else:
+            json_cmd = data
+        cmd = json_cmd[COMMAND]
+        value = str(json_cmd[VALUE])
+        return cmd, value
+
+    @staticmethod
+    def createCmd(cmd, value):
+        data = {COMMAND: cmd, VALUE: str(value)}
+        return json.dumps(data).encode()
+
---
+++ py_client/Window.py
@@ -0,0 +1,86 @@
+import wx
+import wx.lib.agw.knobctrl as KC
+from wx import CheckBox
+
+from py_server.Mechaduino import Mechaduino
+from py_server.settings import Settings
+
+
+class Window(wx.Frame):
+    mech1 = None
+
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, -1, "Тестовое окно")
+        self.SetSize((270, 270))
+
+        s1 = wx.BoxSizer(wx.VERTICAL)
+        l = wx.StaticText(self, wx.NewId(), label="Tune", size=(80, -1), style=wx.TE_CENTER)
+        self.__knob1 = KC.KnobCtrl(self, wx.NewId(), size=(80, 80))
+        self.__knob1._maxvalue = 359
+        self.__knob1.SetAngularRange(0, 359)
+        self.__knob1.SetValue(0)
+        self.__check1 = CheckBox(self, wx.NewId(), label='Manual')
+        self.__check1.Bind(wx.EVT_CHECKBOX, self.__onCheched1)
+        self.__angle1 = wx.TextCtrl(self, wx.NewId(), value="0", size=(80, -1), style=wx.TE_CENTER)
+        self.__knob1.Bind(wx.EVT_MOUSEWHEEL, self.onMouseWheel)
+        s1.Add(l)
+        s1.Add(self.__knob1)
+        s1.Add(self.__angle1)
+        s1.Add(self.__check1)
+
+        s2 = wx.BoxSizer(wx.VERTICAL)
+        l =  wx.StaticText(self, wx.NewId(), label="Load", size=(80, -1), style=wx.TE_CENTER)
+        self.__knob2 = KC.KnobCtrl(self, wx.NewId(), size=(80, 80))
+        self.__knob2._maxvalue = 359
+        self.__knob2.SetAngularRange(0, 359)
+        self.__knob2.SetValue(0)
+        self.__check2 = CheckBox(self, wx.NewId(), label='Manual')
+        self.__check2.Bind(wx.EVT_CHECKBOX, self.__onCheched1)
+        self.__angle2 = wx.TextCtrl(self, wx.NewId(), value="0", size=(80, -1), style=wx.TE_CENTER)
+        self.__knob2.Bind(wx.EVT_MOUSEWHEEL, self.onMouseWheel)
+        s2.Add(l)
+        s2.Add(self.__knob2)
+        s2.Add(self.__angle2)
+        s2.Add(self.__check2)
+
+        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        main_sizer.Add(s1)
+        main_sizer.Add(s2)
+        self.SetSizer(main_sizer)
+        self.Layout()
+
+        self.settings = Settings.getInstance()
+        # self.mech1 = Mechaduino(self.settings.mech1_name, self.settings.mech1_port, self.mech1)
+        # self.mech1.start()
+
+    def onMouseWheel(self, event):
+        o = event.GetEventObject()
+        if o == self.__knob1:
+            if event.GetWheelRotation() > 0:
+                if self.__knob1.GetValue() + 1 > 359:
+                    self.__knob1.SetValue(0)
+                else:
+                    self.__knob1.SetValue(self.__knob1.GetValue() + 1)
+            else:
+                if self.__knob1.GetValue() - 1 < 0:
+                    self.__knob1.SetValue(359)
+                else:
+                    self.__knob1.SetValue(self.__knob1.GetValue() - 1)
+            self.__angle1.SetValue(str(self.__knob1.GetValue()))
+        elif o == self.__knob2:
+            if event.GetWheelRotation() > 0:
+                if self.__knob2.GetValue() + 1 > 359:
+                    self.__knob2.SetValue(0)
+                else:
+                    self.__knob2.SetValue(self.__knob2.GetValue() + 1)
+            else:
+                if self.__knob2.GetValue() - 1 < 0:
+                    self.__knob2.SetValue(359)
+                else:
+                    self.__knob2.SetValue(self.__knob2.GetValue() - 1)
+            self.__angle2.SetValue(str(self.__knob2.GetValue()))
+            # self.mech1.set_angle(str(self.__knob1.GetValue())
+
+    def __onCheched1(self, event):
+        print('click')
+        self.__check1.SetValue(False)
